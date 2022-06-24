87754634b00a4bd3dda290fba98b155bb157a0a0
trapziuz <trapziuz@gmail.com>
2022-06-25 06:33:31 +0900

    commit_1065 0

---
+++ 1065. 한수/1065. 한수.cpp
@@ -0,0 +1,44 @@
+//
+//  1065. 한수.cpp
+//  단계별로 풀어보기 (5)
+//
+//  2022/06/25.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+int arithmetic(int N);
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    
+    N = arithmetic(N);
+    
+    cout << N;
+    
+    return 0;
+}
+
+int arithmetic(int N){
+    int arN = 0;
+    
+    for(int i = 0; i < N; i++){
+        if(i < 99)
+            arN++;
+        else if(i < 999){
+            int a = ((i + 1) % 10) - ((i + 1) / 10) % 10;
+            int b = ((i + 1) / 10) % 10 - (((i + 1) / 10) / 10) % 10;
+            if(a == b) arN++;
+        }
+    }
+    
+    return arN;
+}
